1574c1c6c6aef19a47b0be7b122a1a2b2dfda256
chweeks <chrisharryweeks@gmail.com>
2016-01-29 14:15:02 +0000

    fixed routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 InternationsBackend::Application.routes.draw do
-
-  namespace :api, :defaults => {:format => :json} do
     get    "/users",  to: "users#index"
     post   "/users",  to: "users#create"
     get    "/groups", to: "groups#index"
     post   "/groups", to: "groups#create"
     delete "/users",  to: "users#destroy"
     delete "/groups", to: "groups#destroy"
-    get    "/test",   to: "user#test"
-  end
 end
